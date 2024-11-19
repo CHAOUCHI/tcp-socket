93b1778f4f0c1ce84538cee95805ee43d479263c
CHAOUCHI <massi.chaouchi@gmail.com>
2024-11-19 16:01:40 +0100

    Ajout branches

--- server.c
+++ server.c
@@ -11,6 +11,32 @@
 #define SERVER_PORT 3001
 
 
+// Array of file descriptor socket
+// fill inside the accept thread
+int clients[BUFSIZ];
+int nb_clients = 0;
+int server_fd;
+
+void* accept_thread(){
+    while (1)
+    {
+        /**
+         * 4. accept
+         * ATTEND qu'un demande de connexion arrive d'un client
+         * Le client qui utilise la fonction connect() déclanchera cet accept()
+         */
+        struct sockaddr client_addr;
+        socklen_t len;
+        // printf("accept nuemro : %d\n",nb_clients);
+        // printf("Wait for a client to connect...\n");
+        int client_fd = accept(server_fd,&client_addr,&len); perror("accept()");
+        clients[nb_clients] = client_fd;
+        nb_clients++;
+    }
+    
+}
+
+
 void debug_sleep(){
     // ---------- Debug ---- sleep pour voir les étapes défiler lentement.----------
     printf("\n");
@@ -31,7 +57,7 @@ int main(){
     * - Un port
     * 
     */
-    int socket_id = socket(AF_INET,SOCK_STREAM,0);perror("socket()");
+    server_fd = socket(AF_INET,SOCK_STREAM,0);perror("socket()");
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
         .sin_addr.s_addr = INADDR_ANY,
@@ -55,7 +81,7 @@ int main(){
     /**
      * 2. Bind(liaison) du socket à un file_descriptor
      */
-    bind(socket_id,(struct sockaddr*)&addr,sizeof(addr));perror("bind()");
+    bind(server_fd,(struct sockaddr*)&addr,sizeof(addr));perror("bind()");
 
 
 
@@ -77,7 +103,7 @@ int main(){
      * Lancer l'écoute du serveur
      */
     // listen
-    listen(socket_id,BUFSIZ);perror("listen()");
+    listen(server_fd,BUFSIZ);perror("listen()");
     printf("Server listen to 127.0.0.1:%d\n",SERVER_PORT);
 
 
@@ -90,49 +116,33 @@ int main(){
 
 // debug_sleep();
 
+    pthread_t thread;
+    pthread_create(&thread,NULL,accept_thread,NULL);
+    printf("Thread starts...");
 
-
-
-
-    int count_accept = 0;
-
-    // accept les clients en boucle
+    // rcv en boucle
     while(1){
-
-        /**
-         * 4. accept
-         * ATTEND qu'un demande de connexion arrive d'un client
-         * Le client qui utilise la fonction connect() déclanchera cet accept()
-         */
-        struct sockaddr client_addr;
-        socklen_t len;
-        printf("accept nuemro : %d\n",count_accept);
-        printf("Wait for a client to connect...\n");
-        int client_fd = accept(socket_id,&client_addr,&len); perror("accept()");
-        count_accept++;
-        
-        
-
-
-
-
-
-
-
-
-
-
-
 
         /**
          * 5.  recv
          * Lecture du file descriptor en boucle pour recevoir les requete client
          */
         // Read client socket - recv
-        char buf[BUFSIZ]; memset(buf,0,BUFSIZ);
         // Je remplis le buf avec le contenu du fd
-        recv(client_fd,buf,BUFSIZ,0);
-        perror("recv()");
+        for (int i = 0; i < nb_clients; i++)
+        {
+            char buf[BUFSIZ]; memset(buf,0,BUFSIZ);
+            recv(clients[i],buf,BUFSIZ,0);perror("recv()");
+            printf("%d\n",buf);
+            for (int j = 0; j < nb_clients; j++)
+            {
+                send(clients[j],buf,strlen(buf),0);
+            }
+
+            close(clients[i]);perror("close");
+            /* code */
+        }
+        
 
 
 
@@ -193,7 +203,6 @@ int main(){
 
 
         }
-        close(client_fd);perror("close");
     }
 
 // debug_sleep();
